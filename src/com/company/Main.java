b1c374f57dad15591b4a3ef2b13e3a31ad4be2fc
GrannyPython <karabaevmb@gmail.com>
2015-10-23 16:54:39 +0300

    Now in variable Main Text you will find html-text. Not usual text.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -14,12 +14,9 @@ public class Main {
     //http://www.gazeta.spb.ru/allnews/
     //http://www.fontanka.ru/
 
-    // ArrayList<String> PackOfSettingsGazeta = new ArrayList<String>("http://www.gazeta.spb.ru/allnews/", 9,
-    // "div.materials.nonLine", "h1",  "div#ntext", "i" );
-    // ArrayList<String> PacjOfSettingsFontan = new ArrayList<String>("http://m.fontanka.ru/", 1,
-    // "li.article.switcher-all-news", "h2.itemTitle", "div.itemFullText", "span.itemDateCreated");
 
-    public void main(String[] args) throws Exception {
+
+    public static void main(String[] args) throws Exception {
         WebEntity we = new WebEntity();
         we = we.entityFromCfg("config/config.json");
         //System.out.println(we.toString());
